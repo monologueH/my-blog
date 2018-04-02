99d9902ab378ea332a1d389b0224ce79da528337
zhouhannan <zhouhannan@@163.com>
2018-04-02 22:56:12 +0800

    新增mongoose

--- blogExpress/routes/index.js
+++ blogExpress/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/test", function (err) {
+  if (err) {
+    console.log('连接失败');
+  } else {
+    console.log('连接成功');
+  }
+});
+setTimeout(function () {
+  mongoose.disconnect(function () {
+    console.log("断开连接");
+  })
+}, 2000);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ user: 'tobi' })
 });
 
 module.exports = router;
